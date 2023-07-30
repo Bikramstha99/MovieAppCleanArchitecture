ff11fd600c2faaec95363c3011d2f2d62c2e63da
Bikram Shrestha <stha.bikram999@gmail.com>
2023-07-30 19:36:33 +0545

    GetallandGetByIdApiDone

---
+++ MovieAppAPI/Controllers/MovieController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieAppAPI.ViewModel;
+using MovieAppApplication.Interface.IServices;
+
+namespace MovieAppAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MovieController : Controller
+    {
+        private readonly IMovieService _iMovieService;
+
+        public MovieController(IMovieService iMovieService )
+        {
+            _iMovieService = iMovieService;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var movies= _iMovieService.GetAllMovies();
+            var movieViewModels = new List<MovieVM>();
+            foreach (var movie in movies)
+            {
+                movieViewModels.Add(new MovieVM
+                {
+                    Id = movie.Id,
+                    Name = movie.Name,
+                    Description = movie.Description,
+                    Director = movie.Director,
+                    MoviePhoto = movie.MoviePhoto,
+                    Genre = movie.Genre,
+                });
+            }
+            return Ok(movieViewModels);
+        }
+        [HttpGet("{id}")]
+        public IActionResult Detail(int id)
+        {
+            var movie = _iMovieService.GetByID(id);
+            MovieVM movies = new MovieVM()
+            {
+                Id = movie.Id,
+                Name = movie.Name,
+                Director = movie.Director,
+                Description = movie.Description,
+                MoviePhoto = movie.MoviePhoto,
+                Genre = movie.Genre,
+            };
+            return Ok(movies);
+        }
+    }
+}
---
+++ MovieAppAPI/ViewModel/MovieVM.cs
@@ -0,0 +1,27 @@
+﻿using Nest;
+using System.ComponentModel.DataAnnotations;
+
+namespace MovieAppAPI.ViewModel
+{
+    public class MovieVM
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Genre { get; set; }
+
+        [Required]
+        public string Director { get; set; }
+        [Required]
+        public string Description { get; set; }
+
+
+        [Display(Name = "Insert an Image")]
+        public double AverageRating { get; set; }
+        public string MoviePhoto { get; set; }
+        public List<MovieVM> Movies { get; set; }
+    }
+}
